e4a3cc8d418c56ad6bb0d6802b3c26dfafa2998a
Antoine de Prieelle <antoine_@live.nl>
2021-07-13 16:50:18 +0200

    Add scss styling

--- src/features/weather/CurrentWeather/PreviewCurrentWeather.js
+++ src/features/weather/CurrentWeather/PreviewCurrentWeather.js
@@ -39,34 +39,38 @@ export const PreviewCurrentWeather = () => {
         ToggleWrongLocationTooltip={ToggleWrongLocationTooltip}
       />
 
-      <div className="weather-icon">
-        <img src={`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`} alt="weather-icon"></img>
+      <div className="weather-icon-description-wrapper">
+        <div className="weather-icon">
+          <img src={`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`} alt="weather-icon"></img>
+        </div>
+        <div className="weather-description">{weatherData.current.weather[0].description}</div>
       </div>
       <div className="temp-temp-feels-like-wrapper">
         <div className="temp">{convertTemperatureUnits(weatherTemperatureUnits, weatherData.current.temp)}</div>
         <div className="temp-feels-like">Feels like: {convertTemperatureUnits(weatherTemperatureUnits, weatherData.current.feels_like)}</div>
       </div>
-      <div className="location">
-        <div className="location-text">{weatherCity}, {weatherCountry}
-          <div className="wrong-location-text" onClick={() => {
-            ToggleWrongLocationTooltip();
-          }}>
-            <i className="fas fa-info-circle info-icon"></i>
+      <div className="location-date-time-wrapper">
+        <div className="location">
+          <div className="location-text">{weatherCity}, {weatherCountry}
+            <div className="wrong-location-text" onClick={() => {
+              ToggleWrongLocationTooltip();
+            }}>
+              <i className="fas fa-info-circle info-icon"></i>
+            </div>
           </div>
-        </div>
-      </div>  
-      <div className="date">{formatLocalDateNow(weatherData.current.dt, weatherData.timezone_offset)}</div>
-      <div className="time-gmt">{formatLocalTime(weatherData.current.dt, weatherData.timezone_offset)} ({secondsToGmtHoursAndMinutes(weatherData.timezone_offset)})</div>
-      <div className="weather-description">{weatherData.current.weather[0].description}</div>
+        </div>  
+        <div className="date">{formatLocalDateNow(weatherData.current.dt, weatherData.timezone_offset)}</div>
+        <div className="time-gmt">{formatLocalTime(weatherData.current.dt, weatherData.timezone_offset)} ({secondsToGmtHoursAndMinutes(weatherData.timezone_offset)})</div>
+      </div>
       <div className="wind-cloudiness-humidity-wrapper">
           <div className="wind-direction-beaufort-title">Wind</div>
           <div className="wind-direction-beaufort-value">
             {rotateWindArrowBeaufort(weatherData.current.wind_deg, convertWindSpeedToBeaufort(weatherData.current.wind_speed))}
           </div>
           <div className="cloudiness-title">Cloudiness</div>
-          <div className="cloudiness-value">{weatherData.current.clouds}%</div>
+          <div className="cloudiness-value">{weatherData.current.clouds} %</div>
           <div className="humidity-title">Humidity</div>
-          <div className="humidity-value">{weatherData.current.humidity}%</div>
+          <div className="humidity-value">{weatherData.current.humidity} %</div>
       </div>
       <div className="display-more">
         <Link to="/current-weather">
