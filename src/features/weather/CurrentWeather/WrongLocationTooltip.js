dcc5df00bba325f6fb9192741391f0bc36ff2f3b
Antoine de Prieelle <antoine_@live.nl>
2021-07-29 14:01:14 +0200

    Update WrongLocationTooltip scss styling

--- src/features/weather/CurrentWeather/WrongLocationTooltip.js
+++ src/features/weather/CurrentWeather/WrongLocationTooltip.js
@@ -12,9 +12,9 @@ export const WrongLocationTooltip = ({
         : "wrong-location-tooltip"
       }>
         <div className="wrong-location-tooltip-content">
-          <div>Hint: if you can't find your city try to add the countrycode (and statecode) in the ISO3166 format.</div> 
-          <div>Format: <code>city, countrycode</code> or <code>city, statecode, countrycode</code></div>
-          <div>Example: <code>London,uk</code> or <code>London,GB-LND,uk</code></div>
+          <div className="wrong-location-tooltip-content-text">If you can't find your city try to add the countrycode (and statecode) in the ISO3166 format.</div> 
+          <div>Format: <code>city,countrycode</code> or <code>city,statecode,countrycode</code></div>
+          <div>Example: <code>London,GB</code> or <code>London,GB-LND,GB</code></div>
           <div className="close-button-wrapper">
             <button className="wrong-location-tooltip-close" onClick={() => {ToggleWrongLocationTooltip()}}>Got it!</button>
           </div>
